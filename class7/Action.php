90803331e09b11a40b15c18e1968247f893bb0bd
moon-start <login0516mp4@gmail.com>
2019-12-19 20:40:34 +0800

    OK

---
+++ class7/Action.php
@@ -0,0 +1,32 @@
+<?php
+abstract class Animal {
+  public function sleep(){
+      echo '睡';
+  }
+}
+class Dog extends Animal {
+  public function move(){
+      echo '跑';
+  }
+}
+class Bird extends Animal{
+  public function move()
+  {
+      echo '飛';
+  }
+}
+#############################
+class Action{
+  public function toMove(Animal $obj)
+  {
+      $obj->move();
+  }
+}
+#############################
+$dog = new Dog();
+$bird = new Bird();
+############################
+$action = new Action();
+$action->toMove($dog);
+$action->toMove($bird);
+?>
