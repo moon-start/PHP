f0ff1a029340e2f7da82f7f77cd05010b10dd6a6
moon-start <login0516mp4@gmail.com>
2019-12-19 22:10:42 +0800

    OK

--- ch7-1/QQ.php
+++ ch7-1/QQ.php
@@ -1,5 +1,5 @@
 <?php
-interface Logger {
+interface LGG {
   public function log(string $msg);
 }
 class QQ {
@@ -18,8 +18,8 @@ $app = new QQ;
 // 使用 new class 建立匿名類別
 
 #### new class                   ### 匿名類別
-#### new class implements Logger ### 匿名類別的實作介面
-$app->setQQ(new class implements Logger {
+#### new class implements LGG ### 匿名類別的實作介面
+$app->setQQ(new class implements LGG {
   public function log(string $msg) {
     print($msg);
   }
