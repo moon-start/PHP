56fecbbf8c11bea8a8e2802cc9264df7f66444dd
moon-start <login0516mp4@gmail.com>
2019-12-26 20:09:07 +0800

    All File

---
+++ cd8/classes/AA.php
@@ -0,0 +1,8 @@
+<?php
+//這是 Classes/Member.php
+class AA{
+  public function getAA(){
+    echo "print member list...<br>";
+  }
+}
+?>
---
+++ cd8/index.php
@@ -0,0 +1,8 @@
+<?php
+//這是 index.php
+include_once __DIR__ . "/import.php";
+
+$member = new Member();
+$member->getAA();
+
+?>
