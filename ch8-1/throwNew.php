b5bc40d90627332601aab46d7d6662fca2507dbf
moon-start <login0516mp4@gmail.com>
2020-01-07 21:24:35 +0800

    All File

--- ch8-1/throwNew.php
+++ ch8-1/throwNew.php
@@ -3,11 +3,16 @@
 ## 繼承 Exception 
 class EmailFormatException extends Exception {
   function printMessage(){
+   
+    ###################################
+    #### 例外訊息:E-mail需要包含'@'
+    ####
+    #### Error Message: E-mail需要包含'@'
+    #### Error Code: 0
+    
     echo "例外訊息:" . $this->getMessage() . "<br />";
-    ###
-    ## Error Message: num can not be 0
+    ####
     echo "Error Message: ".$this->getMessage()."<br />";
-    ## Error Code: 100
     echo "Error Code: ".$this->getCode()."<br />";
   }
 }
@@ -15,12 +20,12 @@ function checkEmail($email){
   ### 檢查有無 @
   if (!strpos($email,"@"))
     ##### 拋出 自訂的錯誤類別
-    throw new EmailFormatException("E-mail需要包含'@'");
+    throw new EmailException("E-mail需要包含'@'");
 }
 
 try {
     checkEmail('gmail.com');
-} catch (EmailFormatException $e){
+} catch (EmailException $e){
   $e->printMessage();
 }
 ?>
