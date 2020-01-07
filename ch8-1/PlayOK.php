9e98af43d4a31a8d7c1b097d5e478a0f55fbf144
moon-start <login0516mp4@gmail.com>
2020-01-07 20:45:46 +0800

    All File

--- ch8-1/PlayOK.php
+++ ch8-1/PlayOK.php
@@ -16,7 +16,7 @@ class PlayOK{
   ## Playing musicPlaying music
   use Game, Music{
     //將 Game 的 play 別名成 gamePlay
-    // Game::play as gamePlay;
+    Game::play as gamePlay;
 
     //使用 Music 的 play 功能，取消 Game 的 play 功能！
     Music::play insteadof Game;
