3b031d571636839dcd6866ced7233425be96595c
moon-start <login0516mp4@gmail.com>
2020-01-07 20:43:40 +0800

    All File

--- ch8-1/Play.php
+++ ch8-1/Play.php
@@ -11,9 +11,13 @@ trait Music{
 }
 
 class Play{
+
+
+  ## Playing musicPlaying music
   use Game, Music{
     //將 Game 的 play 別名成 gamePlay
-    Game::play as gamePlay;
+    // Game::play as gamePlay;
+
     //使用 Music 的 play 功能，取消 Game 的 play 功能！
     Music::play insteadof Game;
   }
