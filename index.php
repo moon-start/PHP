60cf51b15cd4cd16b59cc3391f81e8b720dad13b
moon-start <login0516mp4@gmail.com>
2020-01-07 17:53:16 +0800

    All File

--- index.php
+++ index.php
@@ -1,18 +1,10 @@
 <?php
-require_once 'hello022/vendor/autoload.php';
+require_once 'hello/vendor/autoload.php';
 
-use \CarsAA\Car;
+use Cars\Car;
 $mycar = new Car();
 $KK= $mycar->setName("Ford...");
 echo $KK;
 echo $mycar->getName();
- 
-use \CarsAA\CarA;
-$mycar = new CarA();
-$KK= $mycar->setName("Ford...66");
-echo $KK;
-echo $mycar->getName();
-
-
 
 ?>
