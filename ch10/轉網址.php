c981e98b2f43db607f790a5c4c63af3f086c5d8d
moon-start <login0516mp4@gmail.com>
2020-01-14 20:56:59 +0800

    All File

--- ch10/轉網址.php
+++ ch10/轉網址.php
@@ -1,10 +1,11 @@
 <?php
-// ## 1.錯誤..但繼續?
-// header("Location: http://google.com");
-// ## 2.Location會覆蓋上面
-// header("Location: sessionXXX999.php");
+## 1.錯誤..但繼續?
+header("Location: http://google.com");
+## 2.Location會覆蓋上面
+header("Location: sessionXXX999.php");
+
 ## 3.更新
 header("Refresh: 5"); //每五秒更新一次網頁
-## 5.
+## 5.覆蓋...?
 header("Refresh: 5;url=session.php");
 ?>
